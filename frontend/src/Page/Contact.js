5861a4b611a147c0731324f05c965f12ac0e9055
fasil7890 <mohammedfasil7890@gmail.com>
2023-03-14 20:43:29 +0530

    first commit

---
+++ frontend/src/Page/Contact.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div className='m-10 '>
+      <div className='border-orange-900 border-2 items-center w-full p-3  '>
+        <h2 className='font-semibold text-2xl '>Created by</h2>
+        <h4>Mohammed Fasil</h4>
+        <h4>Email : mohammedfasil7890@gmail.com</h4>
+        <h4>Mobile : 9544600918</h4>
+      </div>
+    </div>
+  )
+}
+
+export default Contact
---
+++ frontend/src/redux/index.js
@@ -0,0 +1,12 @@
+import {configureStore} from "@reduxjs/toolkit";
+import userSliceReducer from "./userSlice";
+import productSliceReducer from "./productSlice";
+
+export const Store = configureStore({
+    reducer : {
+        user : userSliceReducer,
+        product : productSliceReducer
+
+    },
+
+})
